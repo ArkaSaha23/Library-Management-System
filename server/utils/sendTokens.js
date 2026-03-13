4a81e47a475c79c98f991513a7e36a6844c67fb4
Arka Saha <arkacomputer2004@gmail.com>
2026-03-14 04:40:48 +0530

    verification of email is added,tested using mongodb and postman

---
+++ server/utils/sendTokens.js
@@ -0,0 +1,13 @@
+export const sendTokens=(user,statusCode,message,res)=>{
+  const token=user.createTokens();
+  res.status(statusCode)
+  .cookie("token",token,{
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000)
+  })
+  .json({
+    success:true,
+    user,
+    message,
+    token,
+  });
+}
