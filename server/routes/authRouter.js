330a9122158fee6a1b630e1347ce96db555a87bd
Arka Saha <arkacomputer2004@gmail.com>
2026-03-14 22:17:24 +0530

    login and logout router and middleware is added,tested using mongodb and postman

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { register,verifyOTP } from "../controllers/authController.js";
+import { login, logout, register,verifyOTP } from "../controllers/authController.js";
 const router=Router();
 router.post('/register',register);
 router.post('/verifyOTP',verifyOTP);
+router.post('/login',login);
+router.get('/logout',logout);
 export default router;
