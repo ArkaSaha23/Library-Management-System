82d63da36f01606cdb3db273db1807e6ead5b46f
Arka Saha <arkacomputer2004@gmail.com>
2026-03-20 03:54:57 +0530

    added Book management System

---
+++ server/models/bookModel.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+const BookSchema=new mongoose.Schema({
+  title:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  author:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  description:{
+    type:String,
+    required:true,
+  },
+  price:{
+    type:Number,
+    required:true
+  },
+  category: {
+    type: String,
+  },
+
+  language: {
+    type: String,
+    default: "English",
+  },
+  quantityAvailable:{
+    type:Number,
+    required:true,
+  },
+  coverImage: {
+    type: String, // URL (Cloudinary / storage)
+  },
+
+  // fileUrl: {
+  //   type: String, // PDF / EPUB file link
+  //   required: true,
+  // },
+
+  fileType: {
+    type: String,
+    enum: ["pdf", "epub", "doc"],
+    default: "pdf",
+  },
+
+  fileSize: {
+    type: Number, // in KB/MB
+  },
+
+  pages: {
+    type: Number,
+  },
+
+  publisher: {
+    type: String,
+  },
+
+  publishedYear: {
+    type: Number,
+  },
+
+  isFree: {
+    type: Boolean,
+    default: true,
+  },
+},
+{
+    timestamps:true,
+});
+export const BookData = mongoose.model("Book",BookSchema);
+
