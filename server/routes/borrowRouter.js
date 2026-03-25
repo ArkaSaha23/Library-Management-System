47842a5ad3fa0223883a227f5aaae3b8709ffc18
Arka Saha <arkacomputer2004@gmail.com>
2026-03-26 04:00:12 +0530

    added the return book function and calculateFine()

--- server/routes/borrowRouter.js
+++ server/routes/borrowRouter.js
@@ -1,9 +1,10 @@
-import { borrowBook, getBorrowedBookByuser } from "../controllers/borrowController.js";
+import { borrowBook, getBorrowedBookByuser, returnBook } from "../controllers/borrowController.js";
 import {Router} from "express";
 import { isAuthenticated, isAuthorised } from "../middlewares/authentication.js";
 const router = Router();
 
 router.post("/borrowBook/:id",isAuthenticated,isAuthorised("Admin", "User"),borrowBook);
-router.get("/getBorrowedBookByuser/:id",isAuthenticated,isAuthorised("Admin"),getBorrowedBookByuser);
+router.put("/returnBook/:id",isAuthenticated,isAuthorised("Admin", "User"),returnBook);
+//router.get("/getBorrowedBookByuser/:id",isAuthenticated,isAuthorised("Admin"),getBorrowedBookByuser);
 
 export default router;
