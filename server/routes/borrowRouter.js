bb6ec23066f9eecb8e6f74d6d8725978b82f5a0c
Arka Saha <arkacomputer2004@gmail.com>
2026-03-26 23:39:54 +0530

    added the renewBook(),getAllBorrowedBooks(),getBorrowedBooksByUser(),seeBorrowedBooks()

--- server/routes/borrowRouter.js
+++ server/routes/borrowRouter.js
@@ -1,10 +1,18 @@
-import { borrowBook, getBorrowedBookByuser, returnBook } from "../controllers/borrowController.js";
+import { borrowBook,getAllBorrowedBooks, getBorrowedBookByuser, renewBook, returnBook, seeBorrowedBook } from "../controllers/borrowController.js";
 import {Router} from "express";
 import { isAuthenticated, isAuthorised } from "../middlewares/authentication.js";
 const router = Router();
 
 router.post("/borrowBook/:id",isAuthenticated,isAuthorised("Admin", "User"),borrowBook);
+
 router.put("/returnBook/:id",isAuthenticated,isAuthorised("Admin", "User"),returnBook);
-//router.get("/getBorrowedBookByuser/:id",isAuthenticated,isAuthorised("Admin"),getBorrowedBookByuser);
+
+router.put("/renewBook/:id",isAuthenticated,isAuthorised("Admin", "User"),renewBook);
+
+router.get("/getAllBorrowedBooks",isAuthenticated,isAuthorised("Admin"),getAllBorrowedBooks);
+
+router.get("/getBorrowedBooksByUser",isAuthenticated,isAuthorised("Admin"),getBorrowedBookByuser);
+
+router.get("/seeBorrowedBooks",isAuthenticated,isAuthorised("Admin","User"),seeBorrowedBook);
 
 export default router;
