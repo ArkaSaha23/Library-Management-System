27e52f0f19de778d669eb8c8b8899788e470dbde
Arka Saha <arkacomputer2004@gmail.com>
2026-04-06 22:13:57 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,23 @@
-import express, { urlencoded } from 'express';
-import {config} from 'dotenv';
-import cookieParser from 'cookie-parser';
-export const app=express();
+import express, { urlencoded } from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+export const app = express();
 import cors from "cors";
-import {connectDB} from './database/db.js';
-import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import { connectDB } from "./database/db.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import authRouter from "./routes/authRouter.js";
 import bookRouter from "./routes/bookRouter.js";
 import borrowRouter from "./routes/borrowRouter.js";
 import userRouter from "./routes/userRouter.js";
-import expressFileupload from "express-fileupload"
-import { notifyUsersAfterDueDate, notifyUsersOneDayAgo } from './services/notifyUsers.js';
+import expressFileupload from "express-fileupload";
+import {
+  notifyUsersAfterDueDate,
+  notifyUsersOneDayAgo,
+} from "./services/notifyUsers.js";
 
 //we have to set up the path of config.env file in app.js..it loads the environment variables from config.env
-config({path:"./config/config.env"});
+
+config({ path: "./config/config.env" });
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
@@ -22,32 +26,30 @@ app.use((req, res, next) => {
 
 //CORS=Cross origin resource sharing
 app.use(
-  cors(
-    {
-      origin:[process.env.FRONTEND_URL],
-      methods:["GET","POST","PUT","DELETE"],
-      credentials:true,
-    }
-))
-app.use(cookieParser());//Reads cookies from client request.
-app.use(express.json());//Allows Express to read JSON data from request body
-app.use(express.urlencoded({extended:true}));//Reads form data submitted from HTML forms.
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use(cookieParser()); //Reads cookies from client request.
+app.use(express.json()); //Allows Express to read JSON data from request body
+app.use(express.urlencoded({ extended: true })); //Reads form data submitted from HTML forms.
 
-app.use(expressFileupload({
-  useTempFiles:true,
-  tempFileDir:"/tmp/",
-}));
+app.use(
+  expressFileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
 
-app.use("/api/v1/auth",authRouter);
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/books", bookRouter);
-app.use("/api/v1/borrow",borrowRouter);
-app.use("/api/v1/user",userRouter);
+app.use("/api/v1/borrow", borrowRouter);
+app.use("/api/v1/user", userRouter);
 notifyUsersOneDayAgo();
 notifyUsersAfterDueDate();
 
-
 connectDB();
 
-
 app.use(errorMiddleware);
-
