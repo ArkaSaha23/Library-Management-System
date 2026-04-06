f5d9285b87a649e62b9e826939c1c51c6b988aa9
Arka Saha <arkacomputer2004@gmail.com>
2026-04-06 22:12:21 +0530

    Slight changes in book schema

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -14,35 +14,42 @@ const BookSchema=new mongoose.Schema({
     type:String,
     required:true,
   },
+  edition:{
+    type:String,
+    trim:true,
+    default:"1st Edition",
+    required:true
+  },
   price:{
     type:Number,
     required:true
   },
   category: {
     type: String,
+    required:true,
   },
 
   language: {
     type: String,
     default: "English",
+    required:true,
   },
+  AddedDate:{
+    type:Date,
+    default:Date.now(),
+  },
+
   quantityAvailable:{
     type:Number,
     required:true,
   },
+  shelfLocation:{
+    type:String,
+    trim:true,
+  },
 
   coverImage: {
     type: String, // URL (Cloudinary / storage)
-  },
-
-  fileType: {
-    type: String,
-    enum: ["pdf", "epub", "doc"],
-    default: "pdf",
-  },
-
-  fileSize: {
-    type: Number, // in KB/MB
   },
 
   pages: {
