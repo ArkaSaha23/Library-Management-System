c8769db9c238b260e450214d70522974d0f9586d
Arka Saha <arkacomputer2004@gmail.com>
2026-04-06 22:14:25 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import { app } from "./app.js";
 import pkg from "cloudinary";
 const { v2: cloudinary } = pkg;
-import dotenv from "dotenv";
-dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
