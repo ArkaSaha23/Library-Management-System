63512ad2019d903d116310b1031482aad669bd16
Arka Saha <arkacomputer2004@gmail.com>
2026-04-19 14:48:18 +0530

    footer

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div className="z-5 flex items-center justify-center bottom-0 w-full h-12 bg-gray-700">
+function Footer() {
+  return (
+    <div className="z-5 flex items-center justify-center bottom-0 w-full h-12 bg-gray-700">
       <p className="text-md font-semibold text-gray-300">
-        &copy; {new Date().getFullYear} Arka Saha. All rights reserved.
+        &copy; {new Date().getFullYear()} ShelfSync. All rights reserved.
       </p>
     </div>
-    )
-  }
+  );
 }
+export default Footer;
