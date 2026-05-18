b94ffd0c2b5aa7688152ba760b86c80550aa2e84
Arka Saha <arkacomputer2004@gmail.com>
2026-05-18 22:07:14 +0530

    Landing page

--- client/src/Home/LandingPage.jsx
+++ client/src/Home/LandingPage.jsx
@@ -3,23 +3,27 @@ import { useSelector } from "react-redux";
 
 import Header from "../layout/Header";
 import SideBar from "../layout/SideBar";
+import MainHeader from "../layout/MainHeader";
 
-import AdminDashboard from "../components/AdminDashboard";
+/*import AdminDashboard from "../components/AdminDashboard";
 import BookManagement from "../components/BookManagement";
 import Catalog from "../components/Catalog";
 import Users from "../components/Users";
 import UserDashboard from "../components/UserDashboard";
-import MyBorrowedBooks from "../components/MyBorrowedBooks";
+import MyBorrowedBooks from "../components/MyBorrowedBooks";*/
+
 import HeroSection from "../layout/HeroSection";
 import Collections from "../layout/Collections";
 import AboutUs from "../layout/AboutUs";
 import Authors from "../layout/Authors";
 import Footer from "../layout/Footer";
 
-const LandingPage = () => {
-  const [isSideBarOpen, setSidebar] = useState(false);
-  const [selectedComponent, setSelectedComponent] = useState(""); //it will store "Dashboard","Users","Books","Catalog","MyBorrowedBooks"
-
+const LandingPage = ({
+  isSideBarOpen,
+  setSidebar,
+  selectedComponent,
+  setSelectedComponent,
+}) => {
   const user = useSelector((state) => state.authReducer.user);
   const isAuthenticated = useSelector(
     (state) => state.authReducer.isAuthenticated,
@@ -31,11 +35,15 @@ const LandingPage = () => {
 
   return (
     <>
-      <Header toggleSidebar={toggleSidebar} />
+      {isAuthenticated ? (
+        <MainHeader toggleSidebar={toggleSidebar} />
+      ) : (
+        <Header toggleSidebar={toggleSidebar} />
+      )}
       <SideBar
         isSideBarOpen={isSideBarOpen}
         setSidebar={setSidebar}
-        SelectedComponent={selectedComponent}
+        selectedComponent={selectedComponent}
         setSelectedComponent={setSelectedComponent}
       />
 
