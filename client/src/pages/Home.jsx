6f2166bc6ba19974d8cb1848b5cba3d6985b05a5
Arka Saha <arkacomputer2004@gmail.com>
2026-05-18 22:08:42 +0530

    Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,30 +4,47 @@ import { useSelector } from "react-redux";
 import Header from "../layout/Header";
 import SideBar from "../layout/SideBar";
 
+import MainHeader from "../layout/MainHeader";
+
 import AdminDashboard from "../components/AdminDashboard";
 import BookManagement from "../components/BookManagement";
 import Catalog from "../components/Catalog";
 import Users from "../components/Users";
 import UserDashboard from "../components/UserDashboard";
 import MyBorrowedBooks from "../components/MyBorrowedBooks";
+
 import HeroSection from "../layout/HeroSection";
 import Collections from "../layout/Collections";
 import AboutUs from "../layout/AboutUs";
 import Authors from "../layout/Authors";
 import Footer from "../layout/Footer";
 
-const Home = () => {
-  const [isSideBarOpen, setSidebar] = useState(false);
-  const [selectedComponent, setSelectedComponent] = useState(""); //it will store "Dashboard","Users","Books","Catalog","MyBorrowedBooks"
-
+const Home = ({
+  isSideBarOpen,
+  setSidebar,
+  selectedComponent,
+  setSelectedComponent,
+}) => {
   const user = useSelector((state) => state.authReducer.user);
-  const isAuthenticated = useSelector((state) => state.authReducer.isAuthenticated);
+  const isAuthenticated = useSelector(
+    (state) => state.authReducer.isAuthenticated,
+  );
 
- 
+  const toggleSidebar = () => {
+    setSidebar((prev) => !prev);
+  };
 
   return (
     <>
-      <div className="relative md:pl-64 flex min-h-screen bg-gray-100 pt-16"> 
+      <MainHeader toggleSidebar={toggleSidebar} />
+
+      <SideBar
+        isSideBarOpen={isSideBarOpen}
+        setSidebar={setSidebar}
+        selectedComponent={selectedComponent}
+        setSelectedComponent={setSelectedComponent}
+      />
+      <div className="relative md:pl-64 flex min-h-screen bg-gray-100 pt-16">
         {(() => {
           if (selectedComponent === "Dashboard") {
             if (user?.role === "User") {
