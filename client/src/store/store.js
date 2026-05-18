b9841ad693cf99a9fa8af2824eb167b975e73913
Arka Saha <arkacomputer2004@gmail.com>
2026-05-18 22:09:37 +0530

    Store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import AuthReducer from "./slices/authSlice.js";
 import PopUpReducer from "./slices/popUpSlice.js";
+
 export const store = configureStore({
   reducer:{
     authReducer : AuthReducer,
