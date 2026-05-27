a9a99adacb299df1418c5ba879ca3a5013b7ffe7
Arka Saha <arkacomputer2004@gmail.com>
2026-05-28 02:41:01 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import OTPverification from "./pages/OTP";
 import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import Register from "./pages/Register";
+import UpdatePassword from "./pages/UpdatePassword"; 
 
 //react toastify : beautifull design Alert message popup
 import { ToastContainer } from "react-toastify";
@@ -43,9 +44,10 @@ const App = () => {
           />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/ResetPassword" element={<ResetPassword />} />
+          <Route path="/ResetPassword/:token" element={<ResetPassword />} />
           <Route path="/OTPverification/:email" element={<OTPverification />} /> 
           <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/UpdatePassword" element={<UpdatePassword />} />
         </Routes>
         <ToastContainer theme="dark" />
       </BrowserRouter>
