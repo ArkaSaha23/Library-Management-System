7b66976d25203e7faba30886feac2cfcbee0129d
Arka Saha <arkacomputer2004@gmail.com>
2026-08-10 23:42:45 +0530

    src/store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import AuthReducer from "./slices/authSlice.js";
 import PopUpReducer from "./slices/popUpSlice.js";
-
+import UserReducer from "./slices/userSlice.js";
 export const store = configureStore({
   reducer:{
     authReducer : AuthReducer,
     popUpReducer : PopUpReducer,
+    userReducer: UserReducer,
   },
 });
