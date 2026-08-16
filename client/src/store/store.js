b2a4aeadc632a9d5958cdeb82aa61897baa16d1a
Arka Saha <arkacomputer2004@gmail.com>
2026-08-16 20:42:22 +0530

    feat: add book and borrow reducers to the store configuration

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,10 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./slices/authSlice.js";
 import PopUpReducer from "./slices/popUpSlice.js";
 import UserReducer from "./slices/userSlice.js";
+import BookReducer from "./slices/bookSlice.js";
+import BorrowReducer from "./slices/borrowSlice.js";
 export const store = configureStore({
   reducer:{
     authReducer : AuthReducer,
     popUpReducer : PopUpReducer,
     userReducer: UserReducer,
+    bookReducer: BookReducer,
+    borrowReducer : BorrowReducer,
   },
 });
