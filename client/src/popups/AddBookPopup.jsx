eb689824e50cd6e3dca3f241839ac87c1a95d709
Arka Saha <arkacomputer2004@gmail.com>
2026-08-21 23:30:53 +0530

    feat: implement AddBookPopup component with dispatch functionality and UI layout

--- client/src/popups/AddBookPopup.jsx
+++ client/src/popups/AddBookPopup.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { FaWindowClose } from "react-icons/fa";
+import { toggleAddBookPopup } from "../store/slices/popUpSlice";
 
 const AddBookPopup = () => {
-  console.log("Add Book");
-  return <></>;
+  const dispatch = useDispatch();
+  
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-5">
+        <div className="w-11/12 rounded-lg bg-white shadow-lg sm:w-1/2">
+          {/* Header */}
+          <div className="flex items-center justify-between rounded-t-lg bg-black px-6 py-4 text-white">
+            <h2 className="text-lg font-bold">Add Book Details</h2>
+
+            <button
+              className="text-xl font-bold text-white cursor-pointer"
+              onClick={() => dispatch(toggleAddBookPopup())}
+            >
+              <FaWindowClose />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddBookPopup;
