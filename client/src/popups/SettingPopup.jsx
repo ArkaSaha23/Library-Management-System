e062953a44fb949df48249cfac08d5f85ab917ce
Arka Saha <arkacomputer2004@gmail.com>
2026-08-21 23:30:26 +0530

    feat: implement SettingPopup component with close functionality

--- client/src/popups/SettingPopup.jsx
+++ client/src/popups/SettingPopup.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import closeIcon from "../assets/close-square.png";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { FaWindowClose } from "react-icons/fa";
+import { toggleSettingPopup } from "../store/slices/popUpSlice";
 
 const SettingPopup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default SettingPopup
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-5">
+        <div className="w-11/12 rounded-lg bg-white shadow-lg sm:w-1/2">
+          {/* Header */}
+          <div className="flex items-center justify-between rounded-t-lg bg-black px-6 py-4 text-white">
+            <h2 className="text-lg font-bold">Settings</h2>
+
+            <button
+              className="text-xl font-bold text-white cursor-pointer"
+              onClick={() => dispatch(toggleSettingPopup())}
+            >
+              <FaWindowClose />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SettingPopup;
