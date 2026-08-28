f455e87fdccbce3c6164fe94fd2a470abb40cd4a
Arka Saha <arkacomputer2004@gmail.com>
2026-08-28 14:53:37 +0530

    feat: adjust ReadBookPopup width for improved layout

--- client/src/popups/ReadBookPopup.jsx
+++ client/src/popups/ReadBookPopup.jsx
@@ -20,7 +20,7 @@ const ReadBookPopup = ({ book,hasReturned }) => {
   return (
     <>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-5">
-        <div className="w-11/12 rounded-lg bg-white shadow-lg sm:w-1/2 md:w-2/3">
+        <div className="w-11/12 rounded-lg bg-white shadow-lg sm:w-1/2 md:w-2/5">
           {/* Header */}
           <div className="flex items-center justify-between rounded-t-lg bg-black px-6 py-4 text-white">
             <h2 className="text-lg font-bold">View Book Info</h2>
